dc64b1f6df3e7748c1a844d3bbd05812e3569e51
Junxiao Shi <git@mail1.yoursunny.com>
2016-01-21 10:11:30 -0700

    trailing whitespace rule

--- rules0.js
+++ rules0.js
@@ -25,5 +25,16 @@ addRule('copyright', function(line, i) {
   }
 });
 
+addRule('trailingws', function(line) {
+  if (/\.md$/.test(this.filename)) {
+    // Trailing whitespace is significant in Markdown.
+    return;
+  }
+
+  if (/\s+$/.test(line)) {
+    this.comment('Trailing whitespace should be avoided.');
+  }
+});
+
 };
 })(exports);

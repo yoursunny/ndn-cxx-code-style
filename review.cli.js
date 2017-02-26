63d38b70a268674ef91023e1238d13d72f944c78
Junxiao Shi <git@mail1.yoursunny.com>
2017-02-26 10:15:55 -0700

    review.cli.js command line options

--- review.cli.js
+++ review.cli.js
@@ -1,6 +1,8 @@
+// nodejs review.cli.js <file-type> <repository> < file
+
 var review = require('./review');
 
 var fs = require('fs');
 var input = fs.readFileSync('/dev/stdin').toString();
-var comments = review.reviewFile(input, '-', '-');
+var comments = review.reviewFile(input, process.argv[2] || 'cpp', process.argv[3] || '-');
 console.log(comments);

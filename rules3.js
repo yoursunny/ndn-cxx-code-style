796a832b6c285d5195b5cf3479e39b364f7615d5
Junxiao Shi <git@mail1.yoursunny.com>
2017-02-26 22:07:48 -0700

    rule 3.30 recognize inline function

--- rules3.js
+++ rules3.js
@@ -5,16 +5,16 @@ addRule('3.30', function(line, i) {
   if (/^ *virtual/.test(line)) {
     this.state = i;
   }
-  if (/;/.test(line)) {
+  if (/;/.test(line) || /^\s*\{\s*$/.test(this.lines[i + 1])) {
     if (this.state > 0) {
-      var m = line.match(/(override|final);/);
+      var m = line.match(/(override|final)($|;)/);
       if (m) {
         this.comment('`virtual` should not be used with `' + m[1] + '`', this.state);
       }
     }
     this.state = 0;
   }
-}, ['hpp']);
+});
 
 };
 })(exports);

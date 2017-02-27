911ed043e7fbd2a6082107e3d9393187c39d7587
Junxiao Shi <git@mail1.yoursunny.com>
2017-02-26 22:27:14 -0700

    rule 3.2

--- rules3.js
+++ rules3.js
@@ -1,6 +1,49 @@
 (function(exports){
 exports.provideRules = function(addRule) {
 
+addRule('3.2', function(line, i) {
+  if (i == 0) {
+    this.state = {
+      ifndef: -1,
+      define: -1,
+      endif: -1,
+      name: ''
+    };
+  }
+  if (i == -1) {
+    if (this.state.ifndef < 0) {
+      this.comment('Include guard is missing.', 0);
+      return;
+    }
+    if (this.state.define < 0) {
+      this.comment('#define is not found.', this.state.ifndef);
+    }
+    if (this.state.endif < 0) {
+      this.comment('#endif is not found or has wrong comment format.', this.state.ifndef);
+    }
+  }
+  var m = line.match(/^#(ifndef|define|endif \/\/) ([A-Z_]+)/);
+  if (!m) {
+    return;
+  }
+  if (this.state.ifndef < 0 && m[1] == 'ifndef') {
+    this.state.ifndef = i;
+    this.state.name = m[2];
+  }
+  else if (this.state.define < 0 && m[1] == 'define' && m[2] == this.state.name) {
+    this.state.define = i;
+    var filename = this.filename.split('/');
+    filename = filename[filename.length - 1];
+    var guardName = '_' + filename.replace(/[^0-9a-z]/gi, '_').toUpperCase();
+    if (m[2].substr(m[2].length - guardName.length) != guardName) {
+      this.comment('Include guard name should end with `' + guardName + '`.');
+    }
+  }
+  else if (this.state.endif < 0 && m[1] == 'endif //' && m[2] == this.state.name) {
+    this.state.endif = i;
+  }
+}, ['hpp']);
+
 addRule('3.30', function(line, i) {
   if (/^ *virtual/.test(line)) {
     this.state = i;

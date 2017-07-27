15a8cd1f5093ebd646ed6ec0d266abfb2ec84f9a
Junxiao Shi <git@mail1.yoursunny.com>
2017-07-26 17:44:12 -0700

    process: force re-score specific change

--- process.cli.js
+++ process.cli.js
@@ -6,8 +6,12 @@ var review = require('./review');
 var minDate = new Date();
 minDate.setDate(minDate.getDate() - config.RECENT_DAYS);
 var after = dateFormat.asString('yyyy-MM-dd hh:mm:ss O', minDate);
+var query = 'is:watched is:open label:Code-Style=0 after:"' + after + '"';
+if (process.argv[2]) {
+  query = 'change:' + process.argv[2];
+}
 
-gerrit.listChanges('is:watched is:open label:Code-Style=0 after:"' + after + '"')
+gerrit.listChanges(query)
 .then(function(changes){
   return Promise.all(changes.map(function(change){
     console.log('fetch', change.id);
